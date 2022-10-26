960fbaaae7670877b6d71d89f12b2695ee25c53f
Willianson Dantas <williansondantas@gmail.com>
2022-10-26 01:05:02 -0300

    feat: tentando entender o requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,13 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+export default class PVP extends Battle {
+  private playerOne: Fighter;
+  private playerTwo: Fighter;
+
+  constructor(playerOne: Fighter, playerTwo: Fighter) {
+    super(playerOne);
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+  }
+}
